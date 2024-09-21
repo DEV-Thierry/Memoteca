f26d5031772abf797d9e05014b895aebae22c676
DEV-Thierry <64818259+DEV-Thierry@users.noreply.github.com>
2024-09-21 04:21:27 +0000

    feat: utilizando diretivas *ngFor e *ngIf para organizar os pensametnos no mural de pensamentos

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus ',
+      autoria: 'Thierry',
+      modelo: 'modelo3'
+    },{
+      conteudo: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+      autoria: 'Thierry',
+      modelo: 'modelo3'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
